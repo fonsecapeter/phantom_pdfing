88f73d009ca5341bcc3dcfbd7977ae04e53fb055
Peter Fonseca <pfonseca@23andme.com>
2018-03-22 11:16:28 -0700

    Ensure unique file name

--- save_page.py
+++ save_page.py
@@ -4,22 +4,40 @@ from subprocess import call
 
 
 def _make_outfile_name(url: str) -> str:
-    file_name = (
-        url
-        .replace('https://', '')
-        .replace('http://', '')
-        .replace('.', '-dot-')
-        .replace('/', '-')
-    )
-    return str(path.join(
-        'out',
-        'py',
-        f'{file_name}-{str(uuid.uuid4())}.pdf'
-    ))
+    found_unique_name = False
+    while not found_unique_name:
+        file_name = (
+            url
+            .replace('https://', '')
+            .replace('http://', '')
+            .replace('.', '-dot-')
+            .replace('/', '-')
+            .replace('?', '-q-')
+            .replace('&', '-and-')
+        )
+        file_path = path.join(
+            'out',
+            'py',
+            f'{file_name}-{str(uuid.uuid4())}.pdf'
+        )
+        found_unique_name = not path.isfile(file_path)
+    return str(file_path)
 
 
 def generate_pdf(url: str) -> None:
     out_file_name = _make_outfile_name(url)
+    # call([
+    #     'phantomjs',
+    #     '--web-security=no',
+    #     '--ssl-protocol=any',
+    #     'rasterize.js',
+    #     url,
+    #     out_file_name,
+    #     'A4',          # paper format
+    #     'portrait',    # orientation
+    #     '1cm',         # margin
+    #     '1'            # zoom
+    # ])
     call([
         'phantomjs',
         '--web-security=no',
@@ -27,13 +45,14 @@ def generate_pdf(url: str) -> None:
         'rasterize.js',
         url,
         out_file_name,
-        'A4',          # paper format
-        'portrait',    # orientation
+        '11in*11in',     # paper size
+        'landscape',   # orientation
         '1cm',         # margin
-        '1'            # zoom
+        '0.5'          # zoom
     ])
 
 
 if __name__ == '__main__':
     # generate_pdf('http://google.com')  # static
-    generate_pdf('http://peterfonseca.gq')  # react
+    # generate_pdf('http://peterfonseca.gq')  # react
+    generate_pdf('https://you.23andme.com/public/lucky-you/')
